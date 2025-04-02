318e530deeb0a4f2204aa1336187ec38df436dff
FunkyJacob <jackholdt@gmail.com>
2025-04-02 12:08:07 +0200

    added the world

---
+++ 2D Animation/Assets/Scripts/ShrekAni2D.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class ShrekAni2D: MonoBehaviour 
+{
+    public float moveSpeed = 5f;
+
+private Rigidbody2D rb;
+private Animator animator;
+private Vector2 movement;
+
+void Start()
+{
+    rb = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+}
+
+void Update()
+{
+    // Reset movement
+    movement = Vector2.zero;
+
+    if (Input.GetKey(KeyCode.D))
+    {
+        movement.x = 1;
+        animator.SetFloat("Run", 1);
+        animator.SetFloat("Dance", 0);
+        animator.SetFloat("Onion", 0);
+        animator.SetFloat("Idle", 0);
+    }
+    else if (Input.GetKey(KeyCode.A))
+    {
+        animator.SetFloat("Dance", 1);
+        animator.SetFloat("Run", 0);
+        animator.SetFloat("Onion", 0);
+        animator.SetFloat("Idle", 0);
+    }
+    else if (Input.GetKey(KeyCode.S))
+    {
+        animator.SetFloat("Onion", 1);
+        animator.SetFloat("Run", 0);
+        animator.SetFloat("Dance", 0);
+        animator.SetFloat("Idle", 0);
+    }
+    else
+    {
+        animator.SetFloat("Idle", 1);
+        animator.SetFloat("Run", 0);
+        animator.SetFloat("Dance", 0);
+        animator.SetFloat("Onion", 0);
+    }
+}
+
+void FixedUpdate()
+{
+    rb.linearVelocity = new Vector2(movement.x * moveSpeed, rb.linearVelocity.y);
+}
+}
--- 2D Animation/Assets/Scripts/ShrekController.cs
+++ 2D Animation/Assets/Scripts/ShrekController.cs
@@ -1,26 +1,50 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ShrekController : MonoBehaviour
 {
     public float flapForce = 5f;
+    public GameObject gameOverText;
+    public AudioClip jumpSound;
+
     private Rigidbody2D rb;
+    private AudioSource audioSource;
+    [HideInInspector] public bool isDead = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+
+        if (gameOverText != null)
+            gameOverText.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!isDead && Input.GetKeyDown(KeyCode.Space))
         {
             rb.linearVelocity = Vector2.up * flapForce;
+
+            audioSource.PlayOneShot(jumpSound);
+            
+        }
+
+        if (isDead && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Time.timeScale = 1f;
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("SHREK IS DEAD 💀");
-        Time.timeScale = 0f; // Freezes game
+        Debug.Log("Shrek just got dunked 💀");
+        isDead = true;
+        Time.timeScale = 0f;
+
+        if (gameOverText != null)
+            gameOverText.SetActive(true);
     }
 }
